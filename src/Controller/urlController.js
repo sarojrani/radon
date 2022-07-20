04f4bfbffd9086c8bd5a907ab2e44900b9ff2beb
Seema 421 <seena_dalal4121@yahoo.com>
2022-07-20 16:39:32 +0530

    jgh

--- src/Controller/urlController.js
+++ src/Controller/urlController.js
@@ -3,25 +3,50 @@ const urlModel = require("../Models/UrlModel");
 const shortId = require("shortid");
 const UrlModel = require("../Models/UrlModel");
 const validator = require("validator");
+const redis = require("redis");
+const { promisify } = require("util");
+
+//Connect to redis
+const redisClient = redis.createClient(
+    13190,
+    "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+);
+redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
+    if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+});
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 //----------------createURL
 
 const urlShorten = async function (req, res) {
     try {
-        
-        let data=req.body
+
+        let data = req.body
         let { longUrl } = data
-        if(!Object.keys(data).length) return res.status(400).send({ status: false, msg: "Please provide url to search" })
-//--------------------URL Validation
+        if (!Object.keys(data).length) return res.status(400).send({ status: false, message: "Please provide url to search" })
+        //--------------------URL Validation
 
-        if (!validator.isURL(longUrl)) 
-        return res.status(400).send({status:false,msg:"Not Valid Url"})
-      
-//----------------------DB Call
+        if (!validator.isURL(longUrl))
+            return res.status(400).send({ status: false, message: "Not Valid Url" })
 
-        let urlFind = await urlModel.findOne({ longUrl })
+        //----------------------DB Call
+
+        let urlFind = await urlModel.find({longUrl}).select({urlCode:0})
+
         console.log(urlFind)
-        if (urlFind) return res.send({ short_url: `${urlFind.shortUrl}` })
+        if (urlFind) return res.status(201).send({status:true,data:urlFind })
         const baseUrl = `https://localhost:3000`
         const urlCode = shortId.generate()
         const shortUrl = baseUrl + '/' + urlCode
@@ -31,26 +56,41 @@ const urlShorten = async function (req, res) {
             urlCode
         })
         let result = await urlModel.create(url)
-        return res.status(201).send({ status: true, msg: result })
+        return res.status(201).send({ status: true, data: result })
 
     } catch (err) {
         res.status(500).send({ status: false, error: err.message })
-    }};
+    }
+};
 
 //-------------------------getAPI
 
 const getUrl = async function (req, res) {
     try {
-        let code=req.params.urlCode
-//---------------urlcode validation
+        let code = req.params.urlCode
+        //---------------urlcode validation
 
-       if(!shortId.isValid(code)) return res.status(400).send({status:false,msg:"Invalid Id"})
-        let result = await urlModel.findOne({ urlCode: req.params.urlCode })
-        if (!result) return res.status(404).send({ status: false, msg: "URL is not present in data base" })
-        if (result) return res.status(302).redirect(result.longUrl )
+        if (!shortId.isValid(code)) return res.status(400).send({ status: false, message: "Invalid Id" })
+        // let result = await urlModel.findOne({ urlCode: req.params.urlCode },{urlCode:1,longUrl:1,shortUrl:1})
+        // if (!result) return res.status(404).send({ status: false, message: "URL is not present in data base" })//
+
+        let cahcedurlData = await GET_ASYNC(`${req.params.urlCode}`)
+     
+        if (cahcedurlData) { res.status(200).send({status:true,data:{urlCode:cahcedurlData.urlCode,longUrl:cahcedurlData.longUrl,shortUrl:cahcedurlData.shortUrl}})}
+         else {
+            let urlData = await urlModel.findOne({ urlCode: req.params.urlCode },{urlCode:1,longUrl:1,shortUrl:1});
+            await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(urlData))
+            res.status(200).send({status:true, data: urlData });
+        }
+
+
+        // let result = await urlModel.findOne({ urlCode: req.params.urlCode })
+        // if (!result) return res.status(404).send({ status: false, message: "URL is not present in data base" })
+        // if (result) return res.status(302).redirect(result.longUrl )
     } catch (err) {
         return res.status(500).send({ status: false, error: err.message })
-    }};
+    }
+};
 
 //--------------------------Exports
 module.exports = { urlShorten, getUrl };
--- src/Models/UrlModel.js
+++ src/Models/UrlModel.js
@@ -5,7 +5,7 @@ const mongoose = require ("mongoose");
 const urlSchema = new mongoose.Schema({
     urlCode: {
         type:String,
-        // required: true,
+        required: true,
         unique: true,
         lowercase:true,
         trim:true
@@ -17,7 +17,7 @@ const urlSchema = new mongoose.Schema({
     },
     shortUrl:{
         type:String,
-        // required:true,
+        required:true,
         unique:true
     }
 },{timestamps:true})
